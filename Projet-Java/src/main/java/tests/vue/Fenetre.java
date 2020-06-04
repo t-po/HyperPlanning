723a82ca3f4be8136892bb60f667a661c2c07d3c
adrienloizeau <adrien.loizeau@edu.Ece>
2020-06-05 00:02:44 +0200

    Ajout de la nav bar et des paramètres

--- Projet-Java/src/main/java/tests/vue/Fenetre.java
+++ Projet-Java/src/main/java/tests/vue/Fenetre.java
@@ -8,26 +8,32 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.BorderLayout;
+import java.awt.Color; 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Hippo
  */
 public class Fenetre extends JFrame{
+   
     
     public Fenetre(){
         this.setLocationRelativeTo(null);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setTitle("Table");
     this.setSize(800, 600);
-
-    
-    
+   
     //Nous ajoutons notre tableau à notre contentPane dans un scroll
     //Sinon les titres des colonnes ne s'afficheront pas !
-    
     this.getContentPane().add(new JScrollPane(tableCreation()));
-    //this.setContentPane(new JScrollPane(new Table()));
     
+    //Creation des parametres en fin de page
+    this.getContentPane().add(parameters(), BorderLayout.SOUTH);
+    this.getContentPane().add(toolBar(), BorderLayout.NORTH);
     this.setBackground(Color.green);
     this.setVisible(true);
     
@@ -81,6 +87,43 @@ public class Fenetre extends JFrame{
     System.out.println("4");
     return tableau;
     }
-
+ 
+public JPanel parameters (){
     
+    /// Formulaire pour rechercher
+    
+    JPanel parametersPan = new JPanel();
+    JButton button = new JButton("Rechercher");
+    JTextField jtf = new JTextField();
+    String[] tab = {"Etudiant", "Professeur", "Salle", "Groupe","Promotion"};
+    JComboBox combo = new JComboBox(tab);
+    JLabel lbl = new JLabel("    Nom: ");
+    
+    /// Création de la boite d'entrée
+    Font police = new Font("Arial", Font.CENTER_BASELINE, 14);
+    jtf.setFont(police);
+    jtf.setPreferredSize(new Dimension(150, 30));
+ 
+    ///Ajout de tous les JElements dans butto
+    parametersPan.add(combo);
+    parametersPan.add(lbl);
+    parametersPan.add(jtf);
+    parametersPan.add(button);
+   
+  return parametersPan;
+}
+private JMenuBar toolBar(){
+    
+  JMenuBar menuBar = new JMenuBar();
+  JMenu planning = new JMenu("Planning");
+  JMenu recap = new JMenu("Recapitulatif");
+  JMenu maj = new JMenu("Mise à jour");
+  JMenu stat = new JMenu("Statistiques");
+  menuBar.add(planning);
+  menuBar.add(recap);
+  menuBar.add(maj);
+  menuBar.add(stat);
+  
+  return  menuBar;
+}
 }
